d216fbe3172c73ec933885e0571bc4b7421d34ee
Sergey Pankov <vacuum@ivacuum.ru>
2017-03-21 03:13:36 +0300

    Политики авторизации

---
+++ src/Policies/WithoutCreate.php
@@ -0,0 +1,11 @@
+<?php namespace Ivacuum\Generic\Policies;
+
+use App\User;
+
+class WithoutCreate extends Base
+{
+    public function create(User $me)
+    {
+        return false;
+    }
+}
---
+++ src/Policies/WithoutCreateAndEdit.php
@@ -0,0 +1,16 @@
+<?php namespace Ivacuum\Generic\Policies;
+
+use App\User;
+
+class WithoutCreateAndEdit extends Base
+{
+    public function create(User $me)
+    {
+        return false;
+    }
+
+    public function edit(User $me)
+    {
+        return false;
+    }
+}
