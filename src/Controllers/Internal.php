2f892e2431b43b06196f6173650dc6012946852a
Sergey Pankov <vacuum@ivacuum.ru>
2017-03-21 22:47:24 +0300

    Заготовки контроллеров

---
+++ src/Controllers/Internal.php
@@ -0,0 +1,32 @@
+<?php namespace Ivacuum\Generic\Controllers;
+
+use Ivacuum\Generic\Services\Telegram;
+
+class Internal extends Controller
+{
+    public function ciBuildNotifier(Telegram $telegram)
+    {
+        $emoji = '';
+        $number = $this->request->input('build.number');
+        $status = strtolower($this->request->input('build.status'));
+        $project = $this->request->input('name');
+        $status_text = '';
+
+        if ($status === 'success') {
+            $emoji = "\xE2\x9C\x85 ";
+        } else {
+            $status_text = " [{$status}]";
+        }
+
+        event(new \Ivacuum\Generic\Events\Stats\Build);
+
+        $telegram->notifyAdmin("{$emoji}{$project} build {$number}{$status_text}");
+
+        return 'ok';
+    }
+
+    public function telegramWebhook()
+    {
+        \Log::info(json_encode($this->request->all()));
+    }
+}
---
+++ src/Controllers/Notifications.php
@@ -0,0 +1,14 @@
+<?php namespace Ivacuum\Generic\Controllers;
+
+class Notifications extends Controller
+{
+    public function index()
+    {
+        $user = $this->request->user();
+        $notifications = $user->notifications;
+
+        $user->markNotificationsAsRead();
+
+        return view($this->view, compact('notifications'));
+    }
+}
