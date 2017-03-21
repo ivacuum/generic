b3e8a228fddfea9e9e0c518a9c996dbee383ab47
Sergey Pankov <vacuum@ivacuum.ru>
2017-03-21 03:14:22 +0300

    Недостающий фасад

---
+++ src/Facades/Form.php
@@ -0,0 +1,11 @@
+<?php namespace Ivacuum\Generic\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Form extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return \Ivacuum\Generic\Utilities\Form::class;
+    }
+}
