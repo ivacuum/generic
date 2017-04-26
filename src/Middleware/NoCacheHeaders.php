8ea83d1650011a91ec6c089e03dfb80a43c86e06
Sergey Pankov <vacuum@ivacuum.ru>
2017-04-26 17:03:15 +0300

    Возможность отключения кэширования ответа

---
+++ src/Middleware/NoCacheHeaders.php
@@ -0,0 +1,37 @@
+<?php namespace Ivacuum\Generic\Middleware;
+
+use Illuminate\Contracts\Foundation\Application;
+
+class NoCacheHeaders
+{
+    /**
+     * The application implementation.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function handle($request, \Closure $next)
+    {
+        if ($this->app->environment('testing')) {
+            return $next($request);
+        }
+
+        /* @var $response \Illuminate\Http\Response */
+        $response = $next($request);
+
+        $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+
+        return $response;
+    }
+}
