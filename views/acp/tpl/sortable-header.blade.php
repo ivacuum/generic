b59c0f3c1077a716d99a38598821e2e0730a5d67
Sergey Pankov <vacuum@ivacuum.ru>
2017-07-13 15:59:23 +0300

    Упрощение создания заголовка для сортировки

---
+++ views/acp/tpl/sortable-header.blade.php
@@ -0,0 +1,14 @@
+<a href="{{ UrlHelper::sort($key, $dir ?? 'desc') }}">
+  @if (isset($svg))
+    @svg ($svg)
+  @else
+    {{ ViewHelper::modelFieldTrans($model_tpl, $key) }}
+  @endif
+  @if ($sort_key === $key)
+    @if ($sort_dir === 'desc')
+      @svg (angle-down)
+    @else
+      @svg (angle-up)
+    @endif
+  @endif
+</a>
