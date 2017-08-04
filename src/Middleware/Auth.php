83cc5d4c83cf5faaa1f33d9019ca4e94bdbe90d9
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-05 01:44:13 +0300

    Выход из неактивных учеток

---
+++ src/Middleware/Auth.php
@@ -0,0 +1,49 @@
+<?php namespace Ivacuum\Generic\Middleware;
+
+use App\User;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Middleware\Authenticate;
+
+class Auth extends Authenticate
+{
+    /**
+     * Обработка входящего запроса
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string[]  ...$guards
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    public function handle($request, \Closure $next, ...$guards)
+    {
+        $this->checkStatus($this->authenticate($guards));
+
+        return $next($request);
+    }
+
+    /**
+     * Проверка активирован ли пользователь
+     *
+     * @param  \App\User $user
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function checkStatus($user)
+    {
+        if (!$this->isUserActive($user)) {
+            \Auth::logout();
+
+            throw new AuthenticationException('Not active.');
+        }
+    }
+
+    /**
+     * @param  \App\User $user
+     * @return bool
+     */
+    protected function isUserActive($user)
+    {
+        return (int) $user->status === User::STATUS_ACTIVE;
+    }
+}
