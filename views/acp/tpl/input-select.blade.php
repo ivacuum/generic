0edaf72575dbe75b9797ed77fab72fac1c1c29cb
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-05 17:50:37 +0300

    Выбор элемента в списке на основе одноименного параметра запроса

--- views/acp/tpl/input-select.blade.php
+++ views/acp/tpl/input-select.blade.php
@@ -2,7 +2,7 @@
   <select {{ $required ? 'required' : '' }} class="form-control" name="{{ $name }}">
     <option value=""></option>
     @foreach ($values as $_value => $_title)
-      <option value="{{ $_value }}" {{ $_value == old($name, $model->{$name}) ? 'selected' : '' }}>{{ $_title }}</option>
+      <option value="{{ $_value }}" {{ $_value == old($name, $model->{$name} ?? Request::input($name)) ? 'selected' : '' }}>{{ $_title }}</option>
     @endforeach
   </select>
 </div>
