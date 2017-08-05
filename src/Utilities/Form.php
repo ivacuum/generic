9dd8f6153f9acf3e173a021d49aa259ca8673227
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-05 18:27:20 +0300

    Подсказка параметров

--- src/Utilities/Form.php
+++ src/Utilities/Form.php
@@ -11,26 +11,46 @@ class Form
         return $this;
     }
 
+    /**
+     * @param  array ...$parameters
+     * @return \Ivacuum\Generic\Utilities\Form\Checkbox
+     */
     public function checkbox(...$parameters)
     {
         return (new Form\Checkbox(...$parameters))->model($this->model);
     }
 
+    /**
+     * @param  array ...$parameters
+     * @return \Ivacuum\Generic\Utilities\Form\Radio
+     */
     public function radio(...$parameters)
     {
         return (new Form\Radio(...$parameters))->model($this->model);
     }
 
+    /**
+     * @param  array ...$parameters
+     * @return \Ivacuum\Generic\Utilities\Form\Select
+     */
     public function select(...$parameters)
     {
         return (new Form\Select(...$parameters))->model($this->model);
     }
 
+    /**
+     * @param  array ...$parameters
+     * @return \Ivacuum\Generic\Utilities\Form\Text
+     */
     public function text(...$parameters)
     {
         return (new Form\Text(...$parameters))->model($this->model);
     }
 
+    /**
+     * @param  array ...$parameters
+     * @return \Ivacuum\Generic\Utilities\Form\Textarea
+     */
     public function textarea(...$parameters)
     {
         return (new Form\Textarea(...$parameters))->model($this->model);
