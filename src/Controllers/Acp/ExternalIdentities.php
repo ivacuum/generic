cb444ee3ce7358f91ba69a00dae2440dcad73a16
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-08 14:28:50 +0300

    Фильтрация социальных учеток

--- src/Controllers/Acp/ExternalIdentities.php
+++ src/Controllers/Acp/ExternalIdentities.php
@@ -1,12 +1,26 @@
 <?php namespace Ivacuum\Generic\Controllers\Acp;
 
 use App\ExternalIdentity as Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class ExternalIdentities extends Controller
 {
     public function index()
     {
-        $models = Model::where('user_id', '<>', 0)->orderBy('id', 'desc')->paginate();
+        $user_id = $this->request->input('user_id');
+        $provider = $this->request->input('provider');
+
+        $models = Model::orderBy('id', 'desc')
+            ->unless(is_null($user_id), function (Builder $query) use ($user_id) {
+                return $query->where('user_id', $user_id);
+            })
+            ->when(is_null($user_id), function (Builder $query) {
+                return $query->where('user_id', '<>', 0);
+            })
+            ->when($provider, function (Builder $query) use ($provider) {
+                return $query->where('provider', $provider);
+            })
+            ->paginate();
 
         return view($this->view, compact('models'));
     }
