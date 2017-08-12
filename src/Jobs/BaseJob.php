85e26e2a9b6e23379d64ea8c46ff7a86ade7e027
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-12 19:23:45 +0300

    Базовая задача

---
+++ src/Jobs/BaseJob.php
@@ -0,0 +1,12 @@
+<?php namespace Ivacuum\Generic\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class BaseJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+}
