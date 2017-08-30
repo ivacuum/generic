065d198b20dec3eb115611ceaf47c5391a1d6df4
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-30 16:04:11 +0300

    Метод default наследуется от родителя

--- src/Utilities/Form/Checkbox.php
+++ src/Utilities/Form/Checkbox.php
@@ -5,19 +5,11 @@ class Checkbox extends Base
     public $name;
     public $type = 'checkbox';
     public $values = [];
-    public $default;
     public $required = false;
 
     public function __construct($name)
     {
         $this->name = $name;
-    }
-
-    public function default($value)
-    {
-        $this->default = $value;
-
-        return $this;
     }
 
     public function html()
