2cf43ee988a59e12531e56fd7424f940a86fe727
Sergey Pankov <vacuum@ivacuum.ru>
2017-09-07 02:42:06 +0300

    Ловушка в виде middleware

---
+++ src/Middleware/SpammerTrap.php
@@ -0,0 +1,23 @@
+<?php namespace Ivacuum\Generic\Middleware;
+
+use Illuminate\Validation\ValidationException;
+
+class SpammerTrap
+{
+    public function handle($request, \Closure $next)
+    {
+        if (!in_array($request->method(), ['POST', 'PUT'])) {
+            return $next($request);
+        }
+
+        if (!$request->filled('mail')) {
+            return $next($request);
+        }
+
+        event(new \Ivacuum\Generic\Events\Stats\SpammerTrapped);
+
+        throw ValidationException::withMessages([
+            'mail' => [trans('auth.spammer_trapped')],
+        ]);
+    }
+}
