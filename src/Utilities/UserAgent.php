a56a16df8a7740e15a30ee9290e88c243ec2f0d0
Sergey Pankov <vacuum@ivacuum.ru>
2017-09-10 04:04:04 +0300

    Улучшение чистки строки браузера

--- src/Utilities/UserAgent.php
+++ src/Utilities/UserAgent.php
@@ -15,8 +15,47 @@ class UserAgent
             'Mozilla/4.0 ',
             'Mozilla/5.0 ',
             ' like Gecko',
+            ' Safari/537.36',
+            '; Trident/4.0',
+            '; Trident/5.0',
+            '; Trident/6.0',
+            '; WOW64',
             'compatible; ',
         ], '', $ua);
+
+        $ua = str_replace([
+            'Windows NT 5.1',
+            'Windows NT 6.0',
+            'Windows NT 6.1',
+            'Windows NT 6.2',
+            'Windows NT 6.3',
+            'Windows NT 10.0',
+            'MSIE 6.0',
+            'MSIE 7.0',
+            'MSIE 8.0',
+            'MSIE 9.0',
+            'MSIE 10.0',
+            'Trident/7.0',
+            '; Win64, x64',
+            '; Win64; x64',
+        ], [
+            'Windows XP',
+            'Windows Vista',
+            'Windows 7',
+            'Windows 8',
+            'Windows 8.1',
+            'Windows 10',
+            'IE6',
+            'IE7',
+            'IE8',
+            'IE9',
+            'IE10',
+            'IE11',
+            ' x64',
+            ' x64',
+        ], $ua);
+
+        $ua = preg_replace('/(; rv:([\d\.]+))\)/', ')', $ua);
 
         return preg_replace('/(AppleWebKit|Gecko)\/([\d\.]+) /', '', $ua);
     }
