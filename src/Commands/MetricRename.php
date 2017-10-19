3f213b31aa2745ba7d6927d69366a56bdc09a60c
Sergey Pankov <vacuum@ivacuum.ru>
2017-10-19 20:41:49 +0300

    Команда переименования метрик

---
+++ src/Commands/MetricRename.php
@@ -0,0 +1,34 @@
+<?php namespace Ivacuum\Generic\Commands;
+
+use App\Metric;
+use Ivacuum\Generic\Commands\Command;
+
+class MetricRename extends Command
+{
+    protected $signature = 'app:metric-rename {from} {to}';
+    protected $description = 'Переименование метрики';
+
+    public function handle()
+    {
+        $to = $this->argument('to');
+        $from = $this->argument('from');
+
+        $metrics = Metric::where('event', $from)
+            ->orderBy('date', 'asc')
+            ->get(['date', 'count']);
+
+        $values = [];
+
+        foreach ($metrics as $metric) {
+            $values[] = sprintf('("%s", "%s", %d)', $metric->date, $to, $metric->count);
+        }
+
+        $rows = sizeof($values);
+
+        \DB::statement('INSERT INTO metrics (`date`, `event`, `count`) VALUES ' . implode(', ', $values) . ' ON DUPLICATE KEY UPDATE `count` = `count` + values(`count`)');
+
+        Metric::where('event', $from)->delete();
+
+        $this->info("Renamed {$from} => {$to} (rows: {$rows})");
+    }
+}
