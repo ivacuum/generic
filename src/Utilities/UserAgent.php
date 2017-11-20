55bb1bd33cb7fbb6869a7c56c97d90b3f6ebfe95
Sergey Pankov <vacuum@ivacuum.ru>
2017-11-20 14:01:10 +0300

    Улучшение чистки строки браузера

--- src/Utilities/UserAgent.php
+++ src/Utilities/UserAgent.php
@@ -10,23 +10,29 @@ class UserAgent
      */
     public static function tidy(string $ua): string
     {
-        // ; .NET CLR \d\.+
-        // " Windows NT 5.2;" = Windows Server 2003 or Windows XP x64
         $ua = str_replace([
             '(KHTML, like Gecko) ',
             'Mozilla/4.0 ',
             'Mozilla/5.0 ',
             ' like Gecko',
             ' Safari/537.36',
+            ' Yowser/2.5',
+            ' (Edition Yx)',
             '; Trident/4.0',
             '; Trident/5.0',
             '; Trident/6.0',
             '; WOW64',
+            '; .NET4.0E',
+            '; .NET4.0C',
+            '; .NET CLR 3.5.30729',
+            '; .NET CLR 3.0.30729',
+            '; .NET CLR 2.0.50727',
             'compatible; ',
         ], '', $ua);
 
         $ua = str_replace([
             'Windows NT 5.1',
+            // 'Windows NT 5.2',
             'Windows NT 6.0',
             'Windows NT 6.1',
             'Windows NT 6.2',
@@ -42,6 +48,7 @@ class UserAgent
             '; Win64; x64',
         ], [
             'Windows XP',
+            // 'Windows Server 2003 or Windows XP x64',
             'Windows Vista',
             'Windows 7',
             'Windows 8',
