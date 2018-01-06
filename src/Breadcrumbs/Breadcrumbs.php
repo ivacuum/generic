b8d40273d6e07c5a23c068b2d1abb585909ca7cd
Sergey Pankov <vacuum@ivacuum.ru>
2018-01-06 05:43:46 +0300

    Возможность удалить последний сегмент цепочки навигации (для админки)

--- src/Breadcrumbs/Breadcrumbs.php
+++ src/Breadcrumbs/Breadcrumbs.php
@@ -9,6 +9,13 @@ class Breadcrumbs
         return $this->breadcrumbs;
     }
 
+    public function pop(): self
+    {
+        array_pop($this->breadcrumbs);
+
+        return $this;
+    }
+
     public function push(string $title, ?string $url = null): self
     {
         $this->breadcrumbs[] = compact('title', 'url');
