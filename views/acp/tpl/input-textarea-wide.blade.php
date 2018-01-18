f56c490647254462f228b4a704a6672d0d077764
Sergey Pankov <vacuum@ivacuum.ru>
2018-01-18 20:35:38 +0300

    Добавление id полей для скриптов

--- views/acp/tpl/input-textarea-wide.blade.php
+++ views/acp/tpl/input-textarea-wide.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
   <label class="{{ $required ? 'input-required' : '' }}">{{ $label ?? ViewHelper::modelFieldTrans($entity, $name) }}</label>
-  <textarea {{ $required ? 'required' : '' }} class="form-control textarea-autosized textarea-borderless-focus js-autosize-textarea {{ $errors->has($name) ? 'is-invalid' : '' }} {{ implode(' ', $classes) }}" name="{{ $name }}" rows="2" placeholder="{{ $placeholder }}">{{ old($name, $model->{$name}) }}</textarea>
+  <textarea {{ $required ? 'required' : '' }} class="form-control textarea-autosized textarea-borderless-focus js-autosize-textarea {{ $errors->has($name) ? 'is-invalid' : '' }} {{ implode(' ', $classes) }}" name="{{ $name }}" rows="2" placeholder="{{ $placeholder }}" id="{{ $entity }}_{{ $name }}">{{ old($name, $model->{$name}) }}</textarea>
   @if ($errors->has($name))
     <div class="invalid-feedback">{{ $errors->first($name) }}</div>
   @endif
