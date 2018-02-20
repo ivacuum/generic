dfa7016407092e75ff5a106c0e6d5e41b9d0b5f6
Sergey Pankov <vacuum@ivacuum.ru>
2018-02-20 05:44:30 +0300

    Автоматическое подключение общих команд

---
+++ src/Providers/CommandsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php namespace Ivacuum\Generic\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommandsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                \Ivacuum\Generic\Commands\MetricDelete::class,
+                \Ivacuum\Generic\Commands\MetricRename::class,
+                \Ivacuum\Generic\Commands\NotificationsPurge::class,
+                \Ivacuum\Generic\Commands\PasswordRemindersPurge::class,
+            ]);
+        }
+    }
+}
