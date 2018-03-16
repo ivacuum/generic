d18c7f71a4513a681e5cb4f422e244523b4566b0
Sergey Pankov <vacuum@ivacuum.ru>
2018-03-16 13:37:29 +0300

    Класс для упрощения работы с кэшированными данными модели

---
+++ src/Utilities/ModelCacheHelper.php
@@ -0,0 +1,78 @@
+<?php namespace Ivacuum\Generic\Utilities;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Вспомогательный класс для упрощения работы с кэшированными данными моделей
+ *
+ * До использования необходимо объявить в экземляре класса:
+ * - const CACHED_BY_ID_KEY
+ * - const CACHED_BY_SLUG_KEY
+ * - protected static $cached_id
+ * - protected static $cached_slug
+ */
+abstract class ModelCacheHelper
+{
+    protected $model;
+    protected $id_field = 'id';
+    protected $order_by = 'title';
+    protected $slug_field = 'slug';
+    protected $title_field = 'title';
+    protected $cached_fields = ['*'];
+    protected $remember_time = 1440;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function cachedById()
+    {
+        return \Cache::remember(static::CACHED_BY_ID_KEY, $this->remember_time, function () {
+            return $this->model->where($this->slug_field, '<>', '')
+                ->orderBy($this->order_by)
+                ->get($this->cached_fields)
+                ->keyBy($this->id_field);
+        });
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function cachedBySlug()
+    {
+        return \Cache::remember(static::CACHED_BY_SLUG_KEY, $this->remember_time, function () {
+            return $this->model->where($this->slug_field, '<>', '')
+                ->orderBy($this->order_by)
+                ->get($this->cached_fields)
+                ->keyBy($this->slug_field);
+        });
+    }
+
+    public function findById(int $id)
+    {
+        if (static::$cached_id === null) {
+            static::$cached_id = $this->cachedById();
+        }
+
+        return isset(static::$cached_id[$id]) ? static::$cached_id[$id] : null;
+    }
+
+    public function findBySlug(?string $slug)
+    {
+        if (!$slug) {
+            return null;
+        }
+
+        if (static::$cached_slug === null) {
+            static::$cached_slug = $this->cachedBySlug();
+        }
+
+        return isset(static::$cached_slug[$slug]) ? static::$cached_slug[$slug] : null;
+    }
+
+    public function title($q): ?string
+    {
+        return is_numeric($q)
+            ? optional($this->findById($q))->{$this->title_field}
+            : optional($this->findBySlug($q))->{$this->title_field};
+    }
+}
