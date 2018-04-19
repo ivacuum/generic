d2b529ad01e4bc002a69be34e273b696052c4106
Sergey Pankov <vacuum@ivacuum.ru>
2018-04-19 17:20:42 +0300

    json-данные под спойлером

--- views/acp/show.blade.php
+++ views/acp/show.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
 @if (Auth::user()->isRoot())
-  <div class="bg-light border mt-3 py-1 px-2">
-    <pre class="d-inline-block text-break-word mb-0 mw-100">{{ $model->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
-  </div>
+  <details class="mt-3">
+    <summary class="outline-0">JSON</summary>
+    <div class="bg-light border mt-1 py-1 px-2 rounded">
+      <pre class="d-inline-block text-break-word mb-0 mw-100">{{ $model->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
+    </div>
+  </details>
 @endif
 @endsection
