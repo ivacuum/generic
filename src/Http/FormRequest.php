286d69090a29106711dabb0eddf3e57a4eb56e71
Sergey Pankov <vacuum@ivacuum.ru>
2018-08-11 15:10:52 +0300

    Заготовка для упрощения валидации форм

---
+++ src/Http/FormRequest.php
@@ -0,0 +1,19 @@
+<?php namespace Ivacuum\Generic\Http;
+
+use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
+
+abstract class FormRequest extends BaseFormRequest
+{
+    protected function validationData()
+    {
+        if (!method_exists($this, 'sanitize')) {
+            return parent::validationData();
+        }
+
+        $data = $this->container->call([$this, 'sanitize'], ['data' => parent::validationData()]);
+
+        $this->replace($data);
+
+        return $data;
+    }
+}
