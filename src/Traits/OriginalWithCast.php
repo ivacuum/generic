8a40c2dcca7bb221f64a402db577483d72a9c56e
Sergey Pankov <vacuum@ivacuum.ru>
2018-09-09 15:44:52 +0300

    Возможность запросить оригинальное значение атрибута модели с учетом типа

---
+++ src/Traits/OriginalWithCast.php
@@ -0,0 +1,17 @@
+<?php namespace Ivacuum\Generic\Traits;
+
+trait OriginalWithCast
+{
+    public function getOriginalWithCast(string $key)
+    {
+        if (null === $value = $this->getOriginal($key)) {
+            return null;
+        }
+
+        if ($this->hasCast($key)) {
+            return $this->castAttribute($key, $value);
+        }
+
+        return $value;
+    }
+}
