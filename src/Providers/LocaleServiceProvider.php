26249a2440f2d6f36ce1a44d711837f65cfa376f
Sergey Pankov <vacuum@ivacuum.ru>
2019-12-04 06:52:19 +0300

    Теперь проекты сами передают локаль сервис провайдеру

--- src/Providers/LocaleServiceProvider.php
+++ src/Providers/LocaleServiceProvider.php
@@ -6,43 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class LocaleServiceProvider extends ServiceProvider
 {
-    public function boot(Request $baseRequest)
+    public function boot(Request $request)
     {
-        $request = $baseRequest->duplicate();
-
-        $locale = $request->segment(1);
-        $locales = config('cfg.locales');
         $defaultLocale = config('app.locale');
-
-        if (is_array($locales) && in_array($locale, array_keys($locales))) {
-            if ($locale !== $defaultLocale) {
-                $baseRequest->server->set('LARAVEL_LOCALE', $locale);
-
-                // /en/news => //news
-                // /en?something => /?something
-                $requestUri = substr_replace($request->getRequestUri(), '', 1, strlen($locale));
-
-                // //news => /news
-                $requestUri = strpos($requestUri, '//') === 0 ? substr_replace($requestUri, '', 0, 1) : $requestUri;
-
-                // Так можно кэшировать маршруты без указания локализации
-                // Но приходится сложнее строить ссылки
-                $baseRequest->server->set('REQUEST_URI', $requestUri);
-            }
-        } else {
-            $locale = $defaultLocale;
-        }
+        $locale = $request->server->get('LARAVEL_LOCALE') ?? $defaultLocale;
 
         setlocale(LC_ALL, config("cfg.locales.{$locale}.posix"));
         setlocale(LC_NUMERIC, 'C');
         Carbon::setLocale($locale);
 
         if ($locale !== $defaultLocale) {
-            \App::setLocale($locale);
+            $this->app->setLocale($locale);
         }
-    }
-
-    public function register()
-    {
     }
 }
