8ee989c60450cf4312f06dc921df4c924f9a0e8d
Sergey Pankov <vacuum@ivacuum.ru>
2020-04-05 11:17:00 +0300

    Поправлен перенос строк

--- views/acp/show.blade.php
+++ views/acp/show.blade.php
@@ -5,7 +5,7 @@
   <details class="mt-4">
     <summary class="outline-none">JSON</summary>
     <div class="bg-light border mt-1 py-1 px-2 text-xs rounded">
-      <pre class="inline-block break-words mb-0 max-w-full">{{ $model->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
+      <pre class="inline-block break-words mb-0 whitespace-pre-wrap max-w-full">{{ $model->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
     </div>
   </details>
 @endif
