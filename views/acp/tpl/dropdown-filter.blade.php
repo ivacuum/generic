ce5772c3f647c8fe94590699868d3a44993dacd3
Sergey Pankov <vacuum@ivacuum.ru>
2020-05-05 23:51:37 +0300

    dropdown не на bootstrap

--- views/acp/tpl/dropdown-filter.blade.php
+++ views/acp/tpl/dropdown-filter.blade.php
@@ -4,25 +4,31 @@
 $current = array_keys($values, request($field))[0];
 $current = mb_strtolower(mb_substr($current, 0, 1)).mb_substr($current, 1);
 ?>
-<div class="dropdown my-1 mr-2 {{ $class ?? '' }}">
-  <a class="btn btn-default dropdown-toggle" href="#" data-toggle="dropdown">
+<details class="relative details-reset details-overlay my-1 mr-2 {{ $class ?? '' }}">
+  <summary class="btn btn-default">
     <span class="text-muted">
       @svg (filter)
     </span>
     {{ $title ?? ViewHelper::modelFieldTrans($modelTpl, $field) }}: {{ $current }}
-  </a>
-  <div class="dropdown-menu">
+    @svg (angle-down)
+  </summary>
+  <details-menu
+    role="menu"
+    class="absolute top-full left-0 z-50 py-2 bg-white mt-1 border border-gray-300 rounded shadow-md"
+    style="min-width: 10rem;"
+  >
     @foreach ($values as $name => $value)
       @if ($name === '---')
-        <div class="dropdown-divider"></div>
+        <div class="h-0 my-2 overflow-hidden border-t border-gray-100"></div>
       @else
         <a
           class="dropdown-item-tw"
           href="{{ UrlHelper::filter([$field => $value]) }}"
+          role="menuitem"
         >
           {{ $name }}
         </a>
       @endif
     @endforeach
-  </div>
-</div>
+  </details-menu>
+</details>
