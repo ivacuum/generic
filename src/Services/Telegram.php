aa6f865758d444315b8b06501caa76566d8ee67b
Sergey Pankov <vacuum@ivacuum.ru>
2021-01-02 18:18:54 +0300

    Не шлем уведомления, если некому слать

--- src/Services/Telegram.php
+++ src/Services/Telegram.php
@@ -6,11 +6,17 @@ class Telegram
 {
     public function notifyAdmin(string $text): void
     {
+        $chatId = config('cfg.telegram.admin_id');
+
+        if ($chatId === null) {
+            return;
+        }
+
         if (\App::isLocal()) {
             $text = "\xF0\x9F\x9A\xA7 local\n{$text}";
         }
 
-        SendTelegramMessageJob::dispatch(config('cfg.telegram.admin_id'), $text, true);
+        SendTelegramMessageJob::dispatch($chatId, $text, true);
     }
 
     public function notifyAdminProduction(string $text): void
