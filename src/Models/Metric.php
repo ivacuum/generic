1532601ba934aa9dad6ef1ebee43a304e29257ab
Sergey Pankov <vacuum@ivacuum.ru>
2023-02-15 23:39:57 +0300

    Исправление ошибки

--- src/Models/Metric.php
+++ src/Models/Metric.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Metric extends Model
 {
+    public $timestamps = false;
     protected $casts = ['date' => 'datetime'];
     protected $perPage = 100;
 
